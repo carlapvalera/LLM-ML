9098df2fc05f74a72a6fac09fcc44b649f0e55f5
carlapvalera <perezvaleracarlas@gmail.com>
2024-09-24 17:06:41 -0400

    add change

--- model/dowload_database.py
+++ model/dowload_database.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 # Cargar el conjunto de datos completo
-dataset = load_dataset("shamanez/RAG-end2end", split="train+validation")  # Cargar ambos splits
+dataset = load_dataset("shamanez/RAG-end2end")  # Cargar ambos splits
 
 # Guardar el conjunto de datos en un directorio local
 dataset.save_to_disk("./rag_dataset")
